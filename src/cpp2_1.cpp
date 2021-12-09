e07d83a93e44bd0ad66bd68dd059623350c7b190
ht19a011 <ht19a011@oecu.jp>
2021-12-09 17:45:33 +0900

    テスト関数の設定

--- src/cpp2_1.cpp
+++ src/cpp2_1.cpp
@@ -42,20 +42,32 @@ int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
 	}return 0;
 };
 
-
+void test(int prime) {
+	int answer[12] = { 92809,6709,12037,103,93523,14503,2,899429,5107,412717,22699,25673 } ;
+	static int count = 0;
+	int counter = count;
+	std::cout << "nth_prime=" << prime << " answer=" << answer[count] ;
+	if (prime == answer[count]) {
+		std::cout << " true" << std::endl;
+	}
+	else {
+		std::cout << " false" << std::endl;
+	}
+	count++;
+}
 
 int main() {
-	std::cout << nth_prime(367, 186, 151) << std::endl;
-	std::cout << nth_prime(179, 10, 203) << std::endl;
-	std::cout << nth_prime(271, 37, 39) << std::endl;
-	std::cout << nth_prime(103, 230, 1) << std::endl;
-	std::cout << nth_prime(27, 104, 185) << std::endl;
-	std::cout << nth_prime(253, 50, 85) << std::endl;
-	std::cout << nth_prime(1, 1, 1) << std::endl;
-	std::cout << nth_prime(9075, 337, 210) << std::endl;
-	std::cout << nth_prime(307, 24, 79) << std::endl;
-	std::cout << nth_prime(331, 221, 177)<< std::endl;
-	std::cout << nth_prime(259, 170, 40) << std::endl;
-	std::cout << nth_prime(269, 58, 102) << std::endl;
+	test(nth_prime(367, 186, 151));
+	test(nth_prime(179, 10, 203));
+	test(nth_prime(271, 37, 39));
+	test(nth_prime(103, 230, 1));
+	test(nth_prime(27, 104, 185));
+	test(nth_prime(253, 50, 85));
+	test(nth_prime(1, 1, 1));
+	test(nth_prime(9075, 337, 210));
+	test(nth_prime(307, 24, 79));
+	test(nth_prime(331, 221, 177));
+	test(nth_prime(259, 170, 40));
+	test(nth_prime(269, 58, 102));
 	//return 0;
 }
